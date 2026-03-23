43cfec60dc9d5d698a4b1b879f9dd9628ff48e02
Rafael Costa <rafael.txct@gmail.com>
2026-03-22 23:31:23 -0300

    chore/Adapting linting preferences and starting logging config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [LoggerModule.forRoot()]
 })
 export class AppModule {}
