1af2a61c2ee61098da1a2636f561f2ea07b7e10b
Rafael Costa <rafael.txct@gmail.com>
2026-03-22 22:15:18 -0300

    chore/Adding logs and cleaning default project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [LoggerModule.forRoot()],
 })
 export class AppModule {}
